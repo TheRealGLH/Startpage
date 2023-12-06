7ad27ce7904cd1a8817601e7e301424bc168d061
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-06 21:45:58 +0100

    Moves sites to json instead of raw html

--- resources/script.js
+++ resources/script.js
@@ -20,6 +20,33 @@ function setGreeting() {
       greeting = "Good morning.";
       break;
   }
-  document.getElementById("greeting").innerHTML = greeting + " It's "+
-   time.getHours()+":"+time.getMinutes()+".";
+  document.getElementById("greeting").innerHTML =
+    greeting + " It's " + time.getHours() + ":" + time.getMinutes() + ".";
+}
+
+function getSites() {
+  fetch("resources/sites.json")
+    .then((response) => response.json())
+    .then((data) => fillItems(data))
+    .catch((error) => console.log(error));
+}
+
+function fillItems(data) {
+  const linkBox = document.querySelector("#linkBox");
+  const template = document.querySelector("#linkBoxItemTemplate");
+  data.sites.forEach((site) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".linkBoxItemText").textContent = site.name;
+    clone.querySelector(".LinkBoxLink").href = site.url;
+    clone.querySelector(".linkBoxItemImage").src =
+      "resources/img/" + site.image_name;
+
+    linkBox.appendChild(clone);
+  });
+  console.log(data.sites[1].name);
+}
+
+function init() {
+  getSites();
+  setGreeting();
 }
